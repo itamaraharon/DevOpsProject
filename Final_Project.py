88398359241b28d5520baee5e31fbc3662a2f8be
itamaraharon <64153048+itamaraharon@users.noreply.github.com>
2020-11-30 08:16:43 +0200

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -136,6 +136,7 @@ def REMOVE_CON():
 def STOP_CON():
     rm = Entry2.get()
     Entry2.delete(0, tk.END)
+    text.delete(0.0, tk.END)
     if rm != '':
         if rm in CONTAINERS():
             try:
@@ -145,7 +146,6 @@ def STOP_CON():
             except():
                 ERROR()
         else:
-            text.delete(0.0, tk.END)
             text.insert(tk.END, "not exist")
     else:
         ENTER_CON()
@@ -176,7 +176,7 @@ lbl1.place(relx=0.5, y=28, anchor='center')
 lbl2 = tk.Label(frame, text='Enter Image:', bg=COLOR_F)
 lbl2.place(x=10, y=60)
 
-lbl3 = tk.Label(frame, text='Enter Container: ', bg=COLOR_F)
+lbl3 = tk.Label(frame, text='Enter Container/image: ', bg=COLOR_F)
 lbl3.place(x=10, y=110)
 
 lbl4 = tk.Label(frame, text='Version: ', bg=COLOR_F)
@@ -196,16 +196,16 @@ btn1 = tk.Button(frame, text="Download", command=lambda: DOWNLOAD_IMAGE())
 btn1.place(x=435, y=60, relheight=0.04, relwidth=0.15)
 
 btn2 = tk.Button(frame, text="run", command=lambda: RUN_CON())
-btn2.place(x=260, y=110, relheight=0.04, relwidth=0.15)
+btn2.place(x=280, y=110, relheight=0.04, relwidth=0.15)
 
 btn3 = tk.Button(frame, text="Delete", command=lambda: REMOVE_IMAGE())
 btn3.place(x=540, y=60, relheight=0.04, relwidth=0.15)
 
 btn4 = tk.Button(frame, text="Delete", command=lambda: REMOVE_CON())
-btn4.place(x=510, y=110, relheight=0.04, relwidth=0.15)
+btn4.place(x=530, y=110, relheight=0.04, relwidth=0.15)
 
 btn4 = tk.Button(frame, text="Stop", command=lambda: STOP_CON())
-btn4.place(x=385, y=110, relheight=0.04, relwidth=0.15)
+btn4.place(x=405, y=110, relheight=0.04, relwidth=0.15)
 
 btn6 = tk.Button(frame, text='show all images', command=lambda: ALL_IMAGES())
 btn6.place(x=460, y=230, relheight=0.08, relwidth=0.25)
